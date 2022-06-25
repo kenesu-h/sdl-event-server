0d8ea3dab8fbbe470c5e49cbdbddc13612c870cf
Kenneth <8515878+kenesu-h@users.noreply.github.com>
2022-06-25 15:42:21 -0400

    Initial commit, server working.

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,51 @@
-pub mod sdl_event;
-pub mod sdl_reader;
-pub mod tcp_writer;
+mod sdl_event;
+mod sdl_reader;
+mod stdio_interface;
 
-use crate::{
-    sdl_event::SdlEvent,
-    sdl_reader::SdlReader,
-    tcp_writer::TcpWriter
-};
+pub use sdl_event::SdlEvent;
+pub use sdl_reader::SdlReader;
+pub use stdio_interface::StdioInterface;
 
-use crossbeam_channel::{tick, select};
+use crossbeam_channel::{tick, select, Receiver};
+use std::time::Instant;
 use std::sync::{
     Arc, Mutex,
     atomic::{AtomicBool, Ordering}
 };
-use std::{thread, time};
+use std::time;
+
+const FREQUENCY: i32 = 60;
 
 fn main() {
-    let mut sdl_reader: SdlReader = SdlReader::new();
-    let mut tcp_writer: TcpWriter = TcpWriter::new();
+    let mut sdl_reader: SdlReader = SdlReader::new(); 
 
-    // Both our main and other thread will read from the same vector.
+    // Our reader and stdio read from the same vector.
     let events_mtx: Arc<Mutex<Vec<SdlEvent>>> = Arc::new(Mutex::new(vec!()));
-    let other_mtx = events_mtx.clone();
+    let events_mtx_stdio = events_mtx.clone();
 
-    // Both threads will run every 1/60s.
-    let ticks = tick(time::Duration::from_secs_f32(1.0 / 60.0));
-    let other_ticks = ticks.clone();
+    // Both will run every 1/60s.
+    let main_ticks: Receiver<Instant> = tick(time::Duration::from_secs_f32(1.0 / FREQUENCY as f32));
+    let stdio_ticks: Receiver<Instant> = main_ticks.clone();
 
-    // Both threads will also end at the same time.
-    let threads_done: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
-    let other_done: Arc<AtomicBool> = threads_done.clone();
+    // Both will also end at the same time.
+    let main_done: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+    let stdio_done: Arc<AtomicBool> = main_done.clone();
 
-    // Run our TCP writer on another thread.
-    let tcp_thread: thread::JoinHandle<_> = thread::spawn(move || {
-        match tcp_writer.accept() {
-            Err(e) => eprintln!("{}", e),
-            Ok(_) => ()
-        }
-        while !threads_done.load(Ordering::Relaxed) {
-            select! {
-                recv(ticks) -> _ => {
-                    if let Ok(mut events) = events_mtx.lock() {
-                        match tcp_writer.write_events(&mut events) {
-                            Err(e) => {
-                                eprintln!("{}", e);
-                                threads_done.store(true, Ordering::SeqCst);
-                            },
-                            Ok(_) => ()
-                        }
-                    }
-                }
-            }
-       }
-    });
+    // We have to run stdio in parallel...
+    let stdio_if: StdioInterface = StdioInterface::new(
+        stdio_ticks, stdio_done, events_mtx_stdio
+    );
 
-    // We MUST run SDL on the main thread.
-    while !other_done.load(Ordering::Relaxed) {
+    // ...but we MUST run SDL on the main thread.
+    while !main_done.load(Ordering::Relaxed) {
         select! {
-            recv(other_ticks) -> _ => {
-                if let Ok(mut events) = other_mtx.lock() {
+            recv(main_ticks) -> _ => {
+                if let Ok(mut events) = events_mtx.lock() {
                     sdl_reader.poll_events(&mut events);
                 }
             }
         }
     }
 
-    match tcp_thread.join() {
-        Err(_) => panic!("Failed to join the TCP thread."),
-        Ok(_) => ()
-    }
+    stdio_if.join();
 }
---
+++ src/stdio_interface.rs
@@ -0,0 +1,121 @@
+use crate::sdl_event::SdlEvent;
+
+use crossbeam_channel::{select, Receiver};
+use serde_json::json;
+use std::{
+    io,
+    io::{BufReader, BufRead, BufWriter, Write},
+    thread,
+    time::Instant,
+    sync::{
+        Arc, Mutex,
+        atomic::{AtomicBool, Ordering}
+    }
+};
+
+struct StdinReader {
+    reader: BufReader<io::Stdin>,
+}
+
+impl StdinReader {
+    pub fn new() -> StdinReader {
+        return StdinReader { reader: BufReader::new(io::stdin()) }
+    }
+
+    pub fn read(&mut self, done: Arc<AtomicBool>) -> () {
+        let mut buffer: String = String::new();
+        match self.reader.read_line(&mut buffer) {
+            Err(_) => done.store(true, Ordering::SeqCst),
+            Ok(_) => ()
+        }
+        match buffer.trim() {
+            "exit" => done.store(true, Ordering::SeqCst),
+            _ => ()
+        }
+    }
+}
+
+struct StdoutWriter {
+    writer: BufWriter<io::Stdout>,
+    error: BufWriter<io::Stderr>
+}
+
+impl StdoutWriter {
+    pub fn new() -> StdoutWriter {
+        return StdoutWriter {
+            writer: BufWriter::new(io::stdout()),
+            error: BufWriter::new(io::stderr())
+        }
+    }
+
+    pub fn write(
+        &mut self, events_mtx: Arc<Mutex<Vec<SdlEvent>>>
+    ) -> Result<(), String> {
+        if let Ok(mut events) = events_mtx.lock() {
+            while let Some(event) = events.pop() {
+                let mut json_string: String = json!(event).to_string();
+                json_string.push('\n');
+                let json: &[u8] = json_string.as_bytes();
+                if let Err(_) = self.writer.write(json) {
+                    return Err(String::from("Failed to write to buffer."));
+                } else {
+                    if let Err(_) = self.writer.flush() {
+                        return Err(String::from("Failed to flush buffer."));
+                    }
+                }
+            }
+        }
+        return Ok(());
+    }
+}
+
+pub struct StdioInterface {
+    read_thread: thread::JoinHandle<()>,
+    write_thread: thread::JoinHandle<()>
+}
+
+impl StdioInterface {
+    pub fn new(
+        ticks: Receiver<Instant>, done: Arc<AtomicBool>,
+        events_mtx: Arc<Mutex<Vec<SdlEvent>>>
+    ) -> StdioInterface {
+        let mut reader: StdinReader = StdinReader::new();
+        let mut writer: StdoutWriter = StdoutWriter::new();
+
+        let read_ticks: Receiver<Instant> = ticks.clone();
+        let write_ticks: Receiver<Instant> = ticks.clone();
+
+        let read_done: Arc<AtomicBool> = Arc::clone(&done);
+        let write_done: Arc<AtomicBool> = Arc::clone(&done);
+
+        let read_thread: thread::JoinHandle<()> = thread::spawn(move || {
+            while !read_done.load(Ordering::Relaxed) {
+                select! {
+                    recv(read_ticks) -> _ => reader.read(read_done.clone())
+                }
+            }
+        });
+
+        let write_thread: thread::JoinHandle<()> = thread::spawn(move || {
+            while !write_done.load(Ordering::Relaxed) {
+                select! {
+                    recv(write_ticks) -> _ => match writer.write(
+                        events_mtx.clone()
+                    ) {
+                        Err(_) => write_done.store(true, Ordering::SeqCst),
+                        Ok(_) => ()
+                    }                
+                }
+            }
+        });
+        return StdioInterface {
+            read_thread: read_thread,
+            write_thread: write_thread
+        }
+    }
+
+    pub fn join(self) -> () {
+        self.read_thread.join().expect("Failed to join read thread.");
+        self.write_thread.join().expect("Failed to join write thread.");
+    }
+}
