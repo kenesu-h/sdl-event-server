1cab1dd5a1fc276e081df98eea2b3b5865541282
Kenneth <8515878+kenesu-h@users.noreply.github.com>
2022-06-26 13:23:53 -0400

    Exported SDLAxis and SDLButton to lib.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod sdl_event;
 mod sdl_reader;
 mod stdio_interface;
 
+pub use sdl_event::SdlAxis;
+pub use sdl_event::SdlButton;
 pub use sdl_event::SdlEvent;
 pub use sdl_reader::SdlReader;
 pub use stdio_interface::StdioInterface;
